076c672cadb35671217427d843e356b106dd111a
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2022-01-15 14:35:07 +0100

    Update FakeRansomware.ps1

--- FakeRansomware.ps1
+++ FakeRansomware.ps1
@@ -1,198 +1,195 @@
 ﻿## IMPORTANT!! your data will NOT be altered. this is fake. check your Documents folder.
 
+#Prepare system
+
+
+    #Create folders
+    $folder_base =  "$env:ProgramData\FakeRan\"
+    $folder_wallpaper = "$folder\Wallpaper"
+    mkdir $folder_wallpaper -ea ignore | Out-Null
+
+
+    Start-Job -Name DownloadWallpaper -ScriptBlock { Invoke-WebRequest "https://i.ibb.co/NKqvy6s/Hardware-manufacturer-Gigabyte-affected-by-ransomware-attack.jpg" -o "C:\wallpaper\skiniw.jpg" } | out-null
+    Wait-Job -Name DownloadWallpaper | out-null
+    $loc = "C:\wallpaper\skiniw.jpg"
+
+
+
 #File encryption
-    $targetdir = [Environment]::GetFolderPath("Desktop")
-    $backupdir = [Environment]::GetFolderPath("MyDocuments") + "\backup"
-    $files = (get-childitem -File $targetdir -Depth 100).FullName | where { ! $_.PSIsContainer }
-    $part1 = Split-Path $files;
-    $fileextension = "cRypt0r"
+$targetdir = [Environment]::GetFolderPath("Desktop")
+$backupdir = [Environment]::GetFolderPath("MyDocuments") + "\backup"
+$files = (get-childitem -File $targetdir -Depth 100).FullName | where { ! $_.PSIsContainer }
+$part1 = Split-Path $files;
+$fileextension = "cRypt0r"
 
 
-    #Copy folder structure
-    Robocopy $targetdir $backupdir /E /XF *.* | out-null
+#Copy folder structure
+Robocopy $targetdir $backupdir /E /XF *.* | out-null
 
-    foreach ($file in $files)
-    {
-    #Ignore files bigger than 1GB
-    if(Get-ChildItem $file  | Where-Object {$_.Length -lt 1gb}){
+foreach ($file in $files)
+{
+#Ignore files bigger than 1GB
+if(Get-ChildItem $file  | Where-Object {$_.Length -lt 1gb}){
 
-    #Copying file from targetdir to backupdir
-    $newdir = $file.Replace($targetdir,$backupdir)
-    Copy-Item $file $newdir
-    Remove-item $file
+#Copying file from targetdir to backupdir
+$newdir = $file.Replace($targetdir,$backupdir)
+Copy-Item $file $newdir
+Remove-item $file
 
-    #Generate fake encrypted file
-    $encrypted = [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($file))
-    $encryptedcontent = $encrypted
-    $encrypted = $encrypted.Substring($encrypted.Length - 16).replace("=","")
-    $encrypted = $encrypted + ".$fileextension"
-    $path = Join-path -path (Split-Path $file) -ChildPath $encrypted
-    new-item $path | out-null
-    Add-Content -Path $path -Value $encryptedcontent
+#Generate fake encrypted file
+$encrypted = [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($file))
+$encryptedcontent = $encrypted
+$encrypted = $encrypted.Substring($encrypted.Length - 16).replace("=","")
+$encrypted = $encrypted + ".$fileextension"
+$path = Join-path -path (Split-Path $file) -ChildPath $encrypted
+new-item $path | out-null
+Add-Content -Path $path -Value $encryptedcontent
 
-    }}
+}}
 
 #Wallpaper
 Function Set-WallPaper {
- 
-<#
- 
-    .SYNOPSIS
-    Applies a specified wallpaper to the current user's desktop
-    
-    .PARAMETER Image
-    Provide the exact path to the image
- 
-    .PARAMETER Style
-    Provide wallpaper style (Example: Fill, Fit, Stretch, Tile, Center, or Span)
-  
-    .EXAMPLE
-    Set-WallPaper -Image "C:\Wallpaper\Default.jpg"
-    Set-WallPaper -Image "C:\Wallpaper\Background.jpg" -Style Fit
-  
-#>
- 
-param (
-    [parameter(Mandatory=$True)]
-    # Provide path to image
-    [string]$Image,
-    # Provide wallpaper style that you would like applied
-    [parameter(Mandatory=$False)]
-    [ValidateSet('Fill', 'Fit', 'Stretch', 'Tile', 'Center', 'Span')]
-    [string]$Style
-)
- 
-$WallpaperStyle = Switch ($Style) {
-  
-    "Fill" {"10"}
-    "Fit" {"6"}
-    "Stretch" {"2"}
-    "Tile" {"0"}
-    "Center" {"0"}
-    "Span" {"22"}
-  
-}
- 
-If($Style -eq "Tile") {
- 
-    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -PropertyType String -Value $WallpaperStyle -Force
-    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -PropertyType String -Value 1 -Force
- 
-}
-Else {
- 
-    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -PropertyType String -Value $WallpaperStyle -Force
-    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -PropertyType String -Value 0 -Force
- 
-}
- 
-Add-Type -TypeDefinition @" 
-using System; 
-using System.Runtime.InteropServices;
-  
-public class Params
-{ 
-    [DllImport("User32.dll",CharSet=CharSet.Unicode)] 
-    public static extern int SystemParametersInfo (Int32 uAction, 
-                                                   Int32 uParam, 
-                                                   String lpvParam, 
-                                                   Int32 fuWinIni);
-}
-"@ 
-  
-    $SPI_SETDESKWALLPAPER = 0x0014
-    $UpdateIniFile = 0x01
-    $SendChangeEvent = 0x02
-  
-    $fWinIni = $UpdateIniFile -bor $SendChangeEvent
-  
-    $ret = [Params]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $Image, $fWinIni)
-}
- 
-Function Set-WallPaper {
- 
-<#
- 
-    .SYNOPSIS
-    Applies a specified wallpaper to the current user's desktop
-    
-    .PARAMETER Image
-    Provide the exact path to the image
- 
-    .PARAMETER Style
-    Provide wallpaper style (Example: Fill, Fit, Stretch, Tile, Center, or Span)
-  
-    .EXAMPLE
-    Set-WallPaper -Image "C:\Wallpaper\Default.jpg"
-    Set-WallPaper -Image "C:\Wallpaper\Background.jpg" -Style Fit
-  
-#>
- 
-param (
-    [parameter(Mandatory=$True)]
-    # Provide path to image
-    [string]$Image,
-    # Provide wallpaper style that you would like applied
-    [parameter(Mandatory=$False)]
-    [ValidateSet('Fill', 'Fit', 'Stretch', 'Tile', 'Center', 'Span')]
-    [string]$Style
-)
- 
-$WallpaperStyle = Switch ($Style) {
-  
-    "Fill" {"10"}
-    "Fit" {"6"}
-    "Stretch" {"2"}
-    "Tile" {"0"}
-    "Center" {"0"}
-    "Span" {"22"}
-  
-}
- 
-If($Style -eq "Tile") {
- 
-    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -PropertyType String -Value $WallpaperStyle -Force
-    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -PropertyType String -Value 1 -Force
- 
-}
-Else {
- 
-    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -PropertyType String -Value $WallpaperStyle -Force
-    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -PropertyType String -Value 0 -Force
- 
-}
- 
-Add-Type -TypeDefinition @" 
-using System; 
-using System.Runtime.InteropServices;
-  
-public class Params
-{ 
-    [DllImport("User32.dll",CharSet=CharSet.Unicode)] 
-    public static extern int SystemParametersInfo (Int32 uAction, 
-                                                   Int32 uParam, 
-                                                   String lpvParam, 
-                                                   Int32 fuWinIni);
-}
-"@ 
-  
-    $SPI_SETDESKWALLPAPER = 0x0014
-    $UpdateIniFile = 0x01
-    $SendChangeEvent = 0x02
-  
-    $fWinIni = $UpdateIniFile -bor $SendChangeEvent
-  
-    $ret = [Params]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $Image, $fWinIni)
-}
- 
 
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Internet Explorer\Main")) {New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Internet Explorer\Main" -Force | Out-Null}
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Internet Explorer\Main" -Name "DisableFirstRunCustomize" -Type DWord -Value 1
-mkdir "c:\wallpaper" | out-null
-Start-Job -Name DownloadWallpaper -ScriptBlock { Invoke-WebRequest "https://i.ibb.co/NKqvy6s/Hardware-manufacturer-Gigabyte-affected-by-ransomware-attack.jpg" -o "C:\wallpaper\skiniw.jpg" } | out-null
-Wait-Job -Name DownloadWallpaper | out-null; sleep -s 3
-Set-WallPaper -Image "C:\wallpaper\skiniw.jpg" -Style Stretch
+<#
+
+.SYNOPSIS
+Applies a specified wallpaper to the current user's desktop
+
+.PARAMETER Image
+Provide the exact path to the image
+
+.PARAMETER Style
+Provide wallpaper style (Example: Fill, Fit, Stretch, Tile, Center, or Span)
+
+.EXAMPLE
+Set-WallPaper -Image "C:\Wallpaper\Default.jpg"
+Set-WallPaper -Image "C:\Wallpaper\Background.jpg" -Style Fit
+
+#>
+
+param (
+[parameter(Mandatory=$True)]
+# Provide path to image
+[string]$Image,
+# Provide wallpaper style that you would like applied
+[parameter(Mandatory=$False)]
+[ValidateSet('Fill', 'Fit', 'Stretch', 'Tile', 'Center', 'Span')]
+[string]$Style
+)
+
+$WallpaperStyle = Switch ($Style) {
+
+"Fill" {"10"}
+"Fit" {"6"}
+"Stretch" {"2"}
+"Tile" {"0"}
+"Center" {"0"}
+"Span" {"22"}
+
+}
+
+If($Style -eq "Tile") {
+
+New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -PropertyType String -Value $WallpaperStyle -Force
+New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -PropertyType String -Value 1 -Force
+
+}
+Else {
+
+New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -PropertyType String -Value $WallpaperStyle -Force
+New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -PropertyType String -Value 0 -Force
+
+}
+
+Add-Type -TypeDefinition @" 
+using System; 
+using System.Runtime.InteropServices;
+
+public class Params
+{ 
+[DllImport("User32.dll",CharSet=CharSet.Unicode)] 
+public static extern int SystemParametersInfo (Int32 uAction, 
+                                               Int32 uParam, 
+                                               String lpvParam, 
+                                               Int32 fuWinIni);
+}
+"@ 
+
+$SPI_SETDESKWALLPAPER = 0x0014
+$UpdateIniFile = 0x01
+$SendChangeEvent = 0x02
+
+$fWinIni = $UpdateIniFile -bor $SendChangeEvent
+
+$ret = [Params]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $Image, $fWinIni)
+}
+
+Function Set-WallPaper {
+
+param (
+[parameter(Mandatory=$True)]
+# Provide path to image
+[string]$Image,
+# Provide wallpaper style that you would like applied
+[parameter(Mandatory=$False)]
+[ValidateSet('Fill', 'Fit', 'Stretch', 'Tile', 'Center', 'Span')]
+[string]$Style
+)
+
+$WallpaperStyle = Switch ($Style) {
+
+"Fill" {"10"}
+"Fit" {"6"}
+"Stretch" {"2"}
+"Tile" {"0"}
+"Center" {"0"}
+"Span" {"22"}
+
+}
+
+If($Style -eq "Tile") {
+
+New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -PropertyType String -Value $WallpaperStyle -Force
+New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -PropertyType String -Value 1 -Force
+
+}
+Else {
+
+New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -PropertyType String -Value $WallpaperStyle -Force
+New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -PropertyType String -Value 0 -Force
+
+}
+
+Add-Type -TypeDefinition @" 
+using System; 
+using System.Runtime.InteropServices;
+
+public class Params
+{ 
+[DllImport("User32.dll",CharSet=CharSet.Unicode)] 
+public static extern int SystemParametersInfo (Int32 uAction, 
+                                               Int32 uParam, 
+                                               String lpvParam, 
+                                               Int32 fuWinIni);
+}
+"@ 
+
+$SPI_SETDESKWALLPAPER = 0x0014
+$UpdateIniFile = 0x01
+$SendChangeEvent = 0x02
+
+$fWinIni = $UpdateIniFile -bor $SendChangeEvent
+
+$ret = [Params]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $Image, $fWinIni)
+}
+
+
+Start-Job -Name DownloadWallpaper -ScriptBlock { Invoke-WebRequest "https://github.com/Andreas6920/FakeRansomware/raw/main/resources/Background.jpg" -o "C:\wallpaper\skiniw.jpg" } | out-null
+Wait-Job -Name DownloadWallpaper | out-null
+$loc = "C:\wallpaper\skiniw.jpg"
+
+
+Set-WallPaper -Image $loc -Style Stretch
 
 
 
@@ -205,37 +202,36 @@ write-host "your data is safe. check $backup_folder" -f Yellow
 
 $intro = 
 '
-                 uuuuuuu
-             uu$$$$$$$$$$$uu 
-          uu$$$$$$$$$$$$$$$$$uu
-         u$$$$$$$$$$$$$$$$$$$$$u
-        u$$$$$$$$$$$$$$$$$$$$$$$u
-       u$$$$$$$$$$$$$$$$$$$$$$$$$u
-       u$$$$$$$$$$$$$$$$$$$$$$$$$u
-       u$$$$$$"   "$$$"   "$$$$$$u
-       "$$$$"      u$u       $$$$"
-        $$$u       u$u       u$$$
-        $$$u      u$$$u      u$$$
-         "$$$$uu$$$   $$$uu$$$$"
-          "$$$$$$$"   "$$$$$$$"
-            u$$$$$$$u$$$$$$$u
-             u$"$"$"$"$"$"$u
-  uuu        $$u$ $ $ $ $u$$       uuu
- u$$$$        $$$$$u$u$u$$$       u$$$$
-  $$$$$uu      "$$$$$$$$$"     uu$$$$$$
+             uuuuuuu
+         uu$$$$$$$$$$$uu 
+      uu$$$$$$$$$$$$$$$$$uu
+     u$$$$$$$$$$$$$$$$$$$$$u
+    u$$$$$$$$$$$$$$$$$$$$$$$u
+   u$$$$$$$$$$$$$$$$$$$$$$$$$u
+   u$$$$$$$$$$$$$$$$$$$$$$$$$u
+   u$$$$$$"   "$$$"   "$$$$$$u
+   "$$$$"      u$u       $$$$"
+    $$$u       u$u       u$$$
+    $$$u      u$$$u      u$$$
+     "$$$$uu$$$   $$$uu$$$$"
+      "$$$$$$$"   "$$$$$$$"
+        u$$$$$$$u$$$$$$$u
+         u$"$"$"$"$"$"$u
+uuu        $$u$ $ $ $ $u$$       uuu
+u$$$$        $$$$$u$u$u$$$       u$$$$
+$$$$$uu      "$$$$$$$$$"     uu$$$$$$
 u$$$$$$$$$$$uu    """""    uuuu$$$$$$$$$$
 $$$$"""$$$$$$$$$$uuu   uu$$$$$$$$$"""$$$"
- """      ""$$$$$$$$$$$uu ""$"""
-           uuuu ""$$$$$$$$$$uuu
-  u$$$uuu$$$$$$$$$uu ""$$$$$$$$$$$uuu$$$
-  $$$$$$$$$$""""           ""$$$$$$$$$$$"
-   "$$$$$"                      ""$$$$""
-     $$$"                         $$$$"
-                                                                                                                                                    
- '
-    1..99 | % {
-        cls; ""; write-host $intro -ForegroundColor White;write "               RANSOMWARE";write "            MuHaHaHaHaHAHAHA!!!";""; Start-Sleep 1;}
-
+"""      ""$$$$$$$$$$$uu ""$"""
+       uuuu ""$$$$$$$$$$uuu
+u$$$uuu$$$$$$$$$uu ""$$$$$$$$$$$uuu$$$
+$$$$$$$$$$""""           ""$$$$$$$$$$$"
+"$$$$$"                      ""$$$$""
+ $$$"                         $$$$"
+                                                                                                                                                
+'
+1..99 | % {
+    cls; ""; write-host $intro -ForegroundColor White; write "               RANSOMWARE";write "            MuHaHaHaHaHAHAHA!!!";""; Start-Sleep 1;}
 
 
 
